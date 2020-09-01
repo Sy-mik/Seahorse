9e16750181c98b4abf47b6ce26235895c98ffec4
Patryk Dziedzic <patrykdziedzic4@gmail.com>
2020-09-01 02:28:04 +0200

    Configure database and authentication in web API project

--- Seahorse.WebApi/Seahorse.WebApi/Startup.cs
+++ Seahorse.WebApi/Seahorse.WebApi/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,10 +10,19 @@ namespace Seahorse.WebApi
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.UseSeahorseDatabase(configuration);
+            services.UseSeahorseAuth();
             services.AddSpaStaticFiles(configuration => configuration.RootPath = "../../web-app/seahorse/build");
         }
 
