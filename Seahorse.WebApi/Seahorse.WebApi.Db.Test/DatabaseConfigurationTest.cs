266b2b93013635ab37e8c694790bd78b3a00f64e
Patryk Dziedzic <patrykdziedzic4@gmail.com>
2020-09-01 02:26:50 +0200

    Add database setup test

---
+++ Seahorse.WebApi/Seahorse.WebApi.Db.Test/DatabaseConfigurationTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Seahorse.WebApi.Db.Service;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Seahorse.WebApi.Db.Test
+{
+    public class DatabaseConfigurationTest
+    {
+        [Fact]
+        public void Configure_AsInMemory_ResultDbContextProviderNameIsInMemory()
+        {
+            //Arrange
+            var serviceCollection = new ServiceCollection();
+            var configuration = GetInMemoryDatabaseConfiguration();
+            serviceCollection.UseSeahorseDatabase(configuration);
+            using ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            //Act
+            using SeahorseDbContext dbContext = serviceProvider.GetRequiredService<SeahorseDbContext>();
+
+            //Assert
+            string expectedProviderName = nameof(Microsoft) +
+                $".{nameof(Microsoft.EntityFrameworkCore)}" +
+                $".{nameof(Microsoft.EntityFrameworkCore.InMemory)}";
+
+            dbContext.Database.ProviderName.Should().Be(expectedProviderName);
+        }
+
+        private IConfiguration GetInMemoryDatabaseConfiguration()
+        {
+            var config = new Dictionary<string, string>
+                {
+                {"Database:DatabaseType", "InMemory"},
+                {"Database:SeahorseDbName", "SeahorseDb"}
+            };
+
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(config)
+                .Build();
+        }
+    }
+}
