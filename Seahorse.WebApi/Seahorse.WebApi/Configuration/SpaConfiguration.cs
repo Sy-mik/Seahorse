de05b52bfeb96164a3fd82bb8379004a4c322420
Patryk Dziedzic <patrykdziedzic4@gmail.com>
2020-09-06 12:10:45 +0200

    Add configuration model for web application paths

---
+++ Seahorse.WebApi/Seahorse.WebApi/Configuration/SpaConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Seahorse.WebApi.Configuration
+{
+    public class SpaConfiguration
+    {
+        public const string Spa = nameof(Spa);
+        public string StaticFilesRootPath { get; set; }
+        public string SourcePath { get; set; }
+    }
+}
