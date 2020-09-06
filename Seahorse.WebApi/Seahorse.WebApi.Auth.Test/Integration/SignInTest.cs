10799b56c46296f15a386e021f74dc0440e3f2d1
Patryk Dziedzic <patrykdziedzic4@gmail.com>
2020-09-06 12:09:56 +0200

    Add authorization integration tests for singing in

---
+++ Seahorse.WebApi/Seahorse.WebApi.Auth.Test/Integration/SignInTest.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Seahorse.WebApi.Auth.Contract;
+using Seahorse.WebApi.Auth.Test.Mock;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Seahorse.WebApi.Auth.Test.Integration
+{
+    public class SignInTest: IClassFixture<WebApplicationMockFactory>
+    {
+        private readonly WebApplicationMockFactory webApplicationFactory;
+
+        public SignInTest(WebApplicationMockFactory webApplicationFactory)
+        {
+            this.webApplicationFactory = webApplicationFactory;
+        }
+
+        [Fact]
+        public async Task SignIn_WithValidCredentials_Returns200()
+        {
+            using var response = await SignInAsync(webApplicationFactory.TestUsers[0]).ConfigureAwait(false);
+            response.StatusCode.Should().Be(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        public async Task SignIn_WithInvalidCredentials_Returns401()
+        {
+            var invalidUser = new ServiceUser("qwerty", "qwerty");
+            using var response = await SignInAsync(invalidUser).ConfigureAwait(false);
+            response.StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
+        }
+
+        [Fact]
+        public async Task SignIn_WithValidCredentials_ReturnsSessionToken()
+        {
+            var signInResult = await GetSignInResultAsync(webApplicationFactory.TestUsers[0]).ConfigureAwait(false);
+            signInResult.Token.Should().NotBeNullOrEmpty();
+        }
+
+        private async Task<HttpResponseMessage> SignInAsync(ServiceUser user)
+        {
+            using var client = webApplicationFactory.CreateClient();
+            var signInRequest = new SignInRequest
+            {
+                Login = user.UserName,
+                Password = user.Password
+            };
+
+            var signInJson = JsonSerializer.Serialize(signInRequest);
+            var jsonContent = new StringContent(signInJson, Encoding.UTF8, "application/json");
+            return await client.PostAsync("/auth/signin", jsonContent).ConfigureAwait(false);
+        }
+
+        private async Task<SignInResult> GetSignInResultAsync(ServiceUser user)
+        {
+            using var response = await SignInAsync(user).ConfigureAwait(false);
+            var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<SignInResult>(responseBody);
+        }
+    }
+}
---
+++ Seahorse.WebApi/Seahorse.WebApi.Auth.Test/Mock/WebApplicationMockFactory.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Seahorse.WebApi.Configuration;
+using Seahorse.WebApi.Contract.Auth;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Seahorse.WebApi.Auth.Test.Mock
+{
+    public class WebApplicationMockFactory : WebApplicationFactory<Startup>
+    {
+        public readonly ServiceUser[] TestUsers = new ServiceUser[]
+        {
+            new ServiceUser("test@test.com", "1qaz@WSX")
+        };
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var spaConfig = new Dictionary<string, string>
+            {
+                [$"{SpaConfiguration.Spa}:{nameof(SpaConfiguration.StaticFilesRootPath)}"] = "../../../../../web-app/seahorse/build",
+                [$"{SpaConfiguration.Spa}:{nameof(SpaConfiguration.SourcePath)}"] = "../../../../../web-app/seahorse"
+            };
+
+            builder.ConfigureAppConfiguration(configBuilder => configBuilder.AddInMemoryCollection(spaConfig));
+            builder.ConfigureServices(ConfigureServices);
+        }
+
+        private void ConfigureServices(IServiceCollection serviceDescriptors)
+        {
+            using var serviceProvider = serviceDescriptors.BuildServiceProvider();
+            using var scope = serviceProvider.CreateScope();
+            AddTestUsers(scope).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private async Task AddTestUsers(IServiceScope scope)
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+            foreach(var user in TestUsers)
+            {
+                await AddUser(userManager, user).ConfigureAwait(false);
+            }
+        }
+
+        private async Task AddUser(UserManager<User> userManager, ServiceUser serviceUser)
+        {
+            var user = new User
+            {
+                UserName = serviceUser.UserName,
+                Email = serviceUser.UserName
+            };
+
+            await userManager.CreateAsync(user, serviceUser.Password).ConfigureAwait(false);
+        }
+    }
+
+    public class ServiceUser
+    {
+        public ServiceUser(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public string UserName { get; }
+        public string Password { get; }
+    }
+}
